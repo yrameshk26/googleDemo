d9d7b4a05b105fa925e8bca43a583f9622e52bd0
yrameshk26 <yrameshk26@gmail.com>
2018-10-22 14:23:06 -0400

    added storing data to google cloud storage

--- src/main/java/com.example.appengine.mail/MailHandlerServlet.java
+++ src/main/java/com.example.appengine.mail/MailHandlerServlet.java
@@ -17,34 +17,60 @@
 package com.example.appengine.mail;
 
 // [START mail_handler_servlet]
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.logging.Logger;
 import java.util.Properties;
 
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.internet.MimeMessage;
+import java.util.Date;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.cloud.storage.*;
+import com.google.cloud.storage.Acl.User;
+import com.google.cloud.storage.Bucket.BucketSourceOption;
+import com.google.cloud.storage.Storage.BlobGetOption;
+
 public class MailHandlerServlet extends HttpServlet {
 
   private static final Logger log = Logger.getLogger(MailHandlerServlet.class.getName());
+  Storage storage = StorageOptions.getDefaultInstance().getService();
+  Date date= new Date();
+  long time = date.getTime();
+  final String bucketName = "telusmailpoc.appspot.com";
 
   @Override
   public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
     Properties props = new Properties();
     Session session = Session.getDefaultInstance(props, null);
+    Bucket bucket = storage.get(bucketName);
+    String blobName = "mail_"+time+".eml";
     try {
       MimeMessage message = new MimeMessage(session, req.getInputStream());
       log.info("Received mail message: " + message.getSubject());
-      log.info("Received mail message: " + message.getContentType());
+      log.info("Received mail message's ContentType: " + message.getContentType());
+      Blob uploadedBlob = uploadMail(bucket, blobName, message);
+      log.info("uploadedBlob : " + uploadedBlob);
     } catch (MessagingException e) {
-     
+      log.info ("MessagingException : " + e);
+    } catch (IOException e1){
+      log.info ("IOException : " + e1);
     }
- 
   }
+
+  public Blob uploadMail(Bucket bucket , String blobName, MimeMessage message) throws IOException, MessagingException {
+    // [START createBlobFromInputStream]
+    InputStream content = message.getInputStream();
+    Blob blob = bucket.create(blobName, content, "multipart/mixed");
+    // [END createBlobFromInputStream]
+    return blob;
+  }
+
 }
 // [END mail_handler_servlet]
