d3d44ffbbb2f50a5b536787eac236bf7f116e76a
yrameshk26 <yrameshk26@gmail.com>
2018-10-23 12:02:00 -0400

    Working eml example. And adding signed url verification

--- src/main/java/com.example.appengine.mail/MailHandlerServlet.java
+++ src/main/java/com.example.appengine.mail/MailHandlerServlet.java
@@ -17,25 +17,32 @@
 package com.example.appengine.mail;
 
 // [START mail_handler_servlet]
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.net.URL;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
-import java.util.Properties;
 
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.internet.MimeMessage;
-import java.util.Date;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.auth.oauth2.AccessToken;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
 import com.google.cloud.storage.*;
 import com.google.cloud.storage.Acl.User;
-import com.google.cloud.storage.Bucket.BucketSourceOption;
-import com.google.cloud.storage.Storage.BlobGetOption;
+import com.google.cloud.storage.Acl.Role;
+import com.google.cloud.storage.Storage.SignUrlOption;
 
 public class MailHandlerServlet extends HttpServlet {
 
@@ -46,17 +53,19 @@ public class MailHandlerServlet extends HttpServlet {
   final String bucketName = "telusmailpoc.appspot.com";
 
   @Override
-  public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+  public void doPost(HttpServletRequest req, HttpServletResponse resp) {
     Properties props = new Properties();
     Session session = Session.getDefaultInstance(props, null);
-    Bucket bucket = storage.get(bucketName);
-    String blobName = "mail_"+time+".eml";
     try {
       MimeMessage message = new MimeMessage(session, req.getInputStream());
+      File eml = File.createTempFile("mail", ".eml");
+      OutputStream out = new FileOutputStream(eml);
+      message.writeTo(out);
+      InputStream emlMessage = new FileInputStream(eml);
+      out.close();
+      eml.delete();
       log.info("Received mail message: " + message.getSubject());
-      log.info("Received mail message's ContentType: " + message.getContentType());
-      Blob uploadedBlob = uploadMail(bucket, blobName, message);
-      log.info("uploadedBlob : " + uploadedBlob);
+      uploadFile(message.getSubject(), bucketName, emlMessage);
     } catch (MessagingException e) {
       log.info ("MessagingException : " + e);
     } catch (IOException e1){
@@ -64,13 +73,34 @@ public class MailHandlerServlet extends HttpServlet {
     }
   }
 
-  public Blob uploadMail(Bucket bucket , String blobName, MimeMessage message) throws IOException, MessagingException {
-    // [START createBlobFromInputStream]
-    InputStream content = message.getInputStream();
-    Blob blob = bucket.create(blobName, content, "multipart/mixed");
-    // [END createBlobFromInputStream]
-    return blob;
+  @SuppressWarnings("deprecation")
+  public void uploadFile(String subject, final String bucketName, InputStream source) throws MessagingException {
+    DateTimeFormatter dtf = DateTimeFormat.forPattern("-YYYY-MM-dd-HHmmssSSS");
+    DateTime dt = DateTime.now(DateTimeZone.UTC);
+    String dtString = dt.toString(dtf);
+    final String fileName = subject +"_" + dtString+".eml";
+
+    // the inputstream is closed by default, so we don't need to close it here
+    BlobInfo blobInfo =
+            storage.create(
+                    BlobInfo
+                            .newBuilder(bucketName, fileName)
+                            // Modify access list to allow all users with link to read file
+//                            .setAcl(new ArrayList<>(Arrays.asList(Acl.of(User.ofAllUsers(), Role.READER))))
+                            .build(),
+                    source);
+
+    // Unsigned download link
+    log.info("uploadedFileurl USUAL: " + blobInfo.getMediaLink());
+
+    // Create ServiceAccountAuthCredentials
+    AccessToken accessToken = new AccessToken("", new Date());
+    GoogleCredentials credentials =
+            GoogleCredentials.create(accessToken);
+    // create signed URL
+    String keyPath = "[PATH_TO_KEY]";
+    URL signedUrl =  storage.get(blobInfo.getBlobId()).signUrl(14,TimeUnit.DAYS, Storage.SignUrlOption.httpMethod(HttpMethod.GET), Storage.SignUrlOption.serviceAccount(credentials));
+    log.info("uploadedFileurl SIGNED: " + signedUrl);
   }
 
 }
-// [END mail_handler_servlet]
